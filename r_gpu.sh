8a348eb054fb070baa6a785932cf7b3c45a100d2
Ijin Yu <iyu1@gh-login03.delta.ncsa.illinois.edu>
2026-03-05 16:18:12 -0600

    r_gpu.sh

--- r_gpu.sh
+++ r_gpu.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=ala-router-h100
+#SBATCH --output=logs/finetune-gpu-%j.out
+#SBATCH --error=logs/finetune-gpu-%j.err
+#SBATCH --partition=ghx4
+#SBATCH --account=benv-dtai-gh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=80G
+#SBATCH --gpus-per-node=1
+#SBATCH --gpu-bind=verbose,closest
+#SBATCH --time=8:00:00
+
+export OMP_NUM_THREADS=16
+export OPENBLAS_NUM_THREADS=16
+export MKL_NUM_THREADS=16
+export NUMEXPR_NUM_THREADS=16
+export VECLIB_MAXIMUM_THREADS=16
+export TOKENIZERS_PARALLELISM=false
+
+export TORCHDYNAMO_DISABLE=1
+
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export CUDA_LAUNCH_BLOCKING=0
+
 # Load compiler + conda
 ##############################
 module purge
